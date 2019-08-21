f6fd1cddc91846c374c88fc979c03cb57dd9394b
wozap kirc <kirc521@gmail.com>
2019-08-21 06:43:14 +0200

    Initial commit

---
+++ room.py
@@ -0,0 +1,13 @@
+class Room:
+    temperature = None
+    humidity = None
+    presence = False
+
+
+class LivingRoom:
+    movieTime = False
+
+class Bedroom:
+    nightTime = False
+    funtime = False
+    moodLights = False
